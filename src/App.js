7b129de0dddaa7fb4513b5b9b9bdff68aeece316
Jéssica <jessicavitorsantos@outlook.com>
2024-01-06 07:57:04 -0300

    base of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import ListCards from './ListCards';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <h1>PARROT CARD GAME</h1>
+      <ListCards />
+    </Content>
   );
 }
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    color: #75B79E;
+    font-size: 45px;
+  }
+`;
 
 export default App;
